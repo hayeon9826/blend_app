898f1bcff377ac2c9dfbf73e64b6bfa5134781bf
hayeon9826 <hayeon226@likelion.org>
2020-04-23 04:03:16 +0900

    댓글 기능 중

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,2 +1,27 @@
 class LecturesController < ApplicationController
+
+  before_action :set_global_search_variable, only: [:index]
+
+  def set_global_search_variable
+    @q = Lecture.ransack(params[:q])
+  end
+
+  def index
+    @recent = Lecture.last(3)
+    @languages = Lecture.where(category: "언어").all
+    @business = Lecture.where(category: 1).all
+    @work = Lecture.where(category: 2).all
+    @codding = Lecture.where(category: 3).all
+    @culture = Lecture.where(category: 4).all
+    @content = Lecture.where(category: 5).all
+    if params[:q]
+      @q = Lecture.ransack(params[:q])
+      @lectures = @q.result(distinct: true)
+      @lectures = @lectures.page(@page).per(12)
+    end
+  end
+
+  def show
+    @lecture = Lecture.find(params[:id])
+  end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,17 @@
 class Lecture < ApplicationRecord
   enum category: {"언어": 0, "창업교육": 1, "직업교육": 2, "코딩": 3, "문화": 4, "콘텐츠 제작": 5}
+
+  def self.create_lectures
+    (1...6).each do |p|
+      Lecture.create!(category: p, title: "Lecture #{p}", address: "Millions of refugees worldwide don't have access to education, career development, and decent jobs.
+As a result of persecution, conflict, violence, human rights violations, or climate change they left behind communities and homes, but refugees have huge potential that is under-utilised. HireChance is on a quest to turn a perceived burden into a impactful opportunity for refugees and the world economy. These are the steps we are taking:")
+    end
+  end
+
+  def self.create_language_lectures
+    (1...6).each do |p|
+      Lecture.create!(category: 0, title: "Lecture #{p}", address: "Millions of refugees worldwide don't have access to education, career development, and decent jobs.
+As a result of persecution, conflict, violence, human rights violations, or climate change they left behind communities and homes, but refugees have huge potential that is under-utilised. HireChance is on a quest to turn a perceived burden into a impactful opportunity for refugees and the world economy. These are the steps we are taking:")
+    end
+  end
 end
