63a0f34ca4c247efc2606bd9895e9eb7b1594c95
hayeon9826 <hayeon226@likelion.org>
2020-04-28 18:50:38 +0900

    deploy to

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:hayeon9826/hayeon9826.git"
 set :passenger_restart_with_touch, true
 set :keep_releases, 2
 
-# set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
