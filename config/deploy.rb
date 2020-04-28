2f01c4c0d7d59ec45bfc6f047b3a5e6dc11237cd
hayeon9826 <hayeon226@likelion.org>
2020-04-28 19:31:09 +0900

    application 지움'

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/home/deploy/blend_app'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/master.key", "config/application.yml"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
