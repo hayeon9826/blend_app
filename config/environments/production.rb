8dc2ed55c6cfe2f0918ad5ccf483aadb536bb72b
hayeon9826 <hayeon226@likelion.org>
2020-05-03 15:19:51 +0900

    production.rb 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+  config.action_mailer.default_url_options = { :host => "www.blendapp2020.com" }
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
