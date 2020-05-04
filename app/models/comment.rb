55c17af1e428c2526b4c242e0d3b7f79c9d0e171
hayeon9826 <hayeon226@likelion.org>
2020-05-05 00:52:53 +0900

    수정

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, optional: true
   belongs_to :user
+
+  def get_lecture
+    lecture = Lecture.find(self.commentable_id)
+    return lecture
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likable, polymorphic: true
+  def get_comments
+    comments = Lecture.find(self.likable_id).comments
+    return comments
+  end
 end
