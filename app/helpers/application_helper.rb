08a59a2d43531f79fb5074cd7de0051eab30de6d
hayeon9826 <hayeon226@likelion.org>
2020-05-24 23:10:53 +0900

    헤더 수정, 사진 추가

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # application_helper.rb
+  def back_link(params, name: "Back", class: "back-button header-title")
+    if params[:back_path].present?
+      link_to name, params[:back_path], class: 'back-button header-title'
+    else
+      link_to name, :back, class: "back-button header-title"
+    end
+  end
 end
